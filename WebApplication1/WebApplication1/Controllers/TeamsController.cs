c7b41fa1c830b5979d666a68719152c10bcf4fcf
deepakk2000 <deepakjc112000@gmal.com>
2023-12-26 18:29:15 +0530

    A-12

---
+++ WebApplication1/WebApplication1/Controllers/TeamsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: Teams
+        static List<Team> listTeam = new List<Team>()
+        {
+            new Team(){Id=1,Name="India",NOWC=10},
+            new Team(){Id=2,Name="Austria",NOWC=4},
+            new Team(){Id=3,Name="West Indies",NOWC=2},
+            new Team(){Id=4,Name="England",NOWC=1},
+        };
+        public ActionResult Index()
+        {
+            return View(listTeam);
+        }
+    }
+}
